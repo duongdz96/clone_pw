83e8abf97cada2691c76334bc3f352917088d3b1
s1mple love <mynameduongnguyen@gmail.com>
2024-04-01 23:07:19 +0700

    xoa product, manufacture

--- admin/connect.php
+++ admin/connect.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
-$connect = mysqli_connect('localhost', 'root', '123456', 'clonepw');
+$connect = mysqli_connect('localhost', 'root', '', 'clonepw');
 mysqli_set_charset($connect, 'utf8');
