ec78dad5234898d8cd8566f48925f73f57d10db4
s1mple love <mynameduongnguyen@gmail.com>
2024-04-01 23:29:07 +0700

    long test di

--- admin/root/index.php
+++ admin/root/index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 </head>
+
 <body>
-Đây là giao diện admin123
-<?php 
-include '../menu.php';
- ?>
+	Đây là giao diện admin
+	<?php
+	include '../menu.php';
+	?>
 </body>
+
 </html>
