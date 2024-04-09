03c100e97e2e5f65222218163699c75290576945
s1mple love <mynameduongnguyen@gmail.com>
2024-04-10 00:01:29 +0700

    delete exam

--- admin/exam/delete.php
+++ admin/exam/delete.php
@@ -1,5 +1,5 @@
-<?php 
-include '../check_admin_login.php';
+<?php
+// include '../check_admin_login.php';
 include '../connect.php';
 
 $id = $_GET['id'];
@@ -9,6 +9,11 @@ where id = '$id'";
 
 mysqli_query($connect, $sql);
 
+echo "Xoa bai thi thanh cong";
+$result = mysqli_query($connect, "SELECT id, name, description FROM exam");
+$data = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
 mysqli_close($connect);
-
-header('location:index.php?success=Xóa thành công');
+echo json_encode($data);
